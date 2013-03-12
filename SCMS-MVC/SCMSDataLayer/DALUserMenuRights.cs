f0e708a45cdff7bf328843999c61e1d216200c0b
Waqas Akhtar <waqas.akhtar@softmatics.com>
2013-03-13 04:31:51 +0500

    Change to User Menu Rights

---
+++ SCMS-MVC/SCMSDataLayer/DALUserMenuRights.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SCMSDataLayer.DB;
+
+namespace SCMSDataLayer
+{
+    public class DALUserMenuRights
+    {
+        public List<sp_GetUserMenuRightsResult> GetUserMenuRights(string GourpId)
+        {
+            SCMSDataContext dbSCMS = Connection.Create();
+            return dbSCMS.sp_GetUserMenuRights(GourpId).ToList();
+        }
+    }
+}
