05d36689ad4160ea591157143ce8acf111bacb96
unknown <awais@nevisol.com>
2013-03-14 22:00:15 +0500

    MA - REPORT SELECTION CRIYERIA - 14Mar2013 10PM

--- SCMS-MVC/SCMS/Controllers/UserRightsSetupController.cs
+++ SCMS-MVC/SCMS/Controllers/UserRightsSetupController.cs
@@ -22,38 +22,38 @@ namespace SCMS.Controllers
 
         public string GetUserMenus(string GroupId)
         {
-            List<sp_GetUserMenuRightsResult> MenuRights = new DALUserMenuRights().GetUserMenuRights(GroupId).OrderBy(c => c.Mnu_Level).ToList();
+            //List<sp_GetUserMenuRightsResult> MenuRights = new DALUserMenuRights().GetUserMenuRights(GroupId).OrderBy(c => c.Mnu_Level).ToList();
             string UserMenuList = "";
-            if (MenuRights != null && MenuRights.Count > 0)
-            {
-                int count = 0;
-                UserMenuList += "<ul>";
-                foreach (sp_GetUserMenuRightsResult row in MenuRights)
-                {
-                    count++;
-                    string node = row.Mnu_Level.Contains(".") ? "Child" : "Parent";
-                    if (node == "Parent")
-                    {
-                        if (UserMenuList.Length > 4)
-                        {
-                            UserMenuList += "</ul>";
-                            UserMenuList += "</li>";
-                        }
-                        UserMenuList += "<li item-checked='" + (row.SelectedMenu > 0 ? "true" : "false") + "' item-expanded='true'>" + row.Mnu_Description;
-                        UserMenuList += "<ul>";
-                    }
-                    else if (node == "Child")
-                    {
-                        UserMenuList += "<li item-checked='" + (row.SelectedMenu > 0 ? "true" : "false") + "'>" + row.Mnu_Description;
-                        if (count == MenuRights.Count)
-                        {
-                            UserMenuList += "</ul>";
-                            UserMenuList += "</li>";
-                        }
-                    }
-                }
-                UserMenuList += "</ul>";
-            }
+            //if (MenuRights != null && MenuRights.Count > 0)
+            //{
+            //    int count = 0;
+            //    UserMenuList += "<ul>";
+            //    foreach (sp_GetUserMenuRightsResult row in MenuRights)
+            //    {
+            //        count++;
+            //        string node = row.Mnu_Level.Contains(".") ? "Child" : "Parent";
+            //        if (node == "Parent")
+            //        {
+            //            if (UserMenuList.Length > 4)
+            //            {
+            //                UserMenuList += "</ul>";
+            //                UserMenuList += "</li>";
+            //            }
+            //            UserMenuList += "<li item-checked='" + (row.SelectedMenu > 0 ? "true" : "false") + "' item-expanded='true'>" + row.Mnu_Description;
+            //            UserMenuList += "<ul>";
+            //        }
+            //        else if (node == "Child")
+            //        {
+            //            UserMenuList += "<li item-checked='" + (row.SelectedMenu > 0 ? "true" : "false") + "'>" + row.Mnu_Description;
+            //            if (count == MenuRights.Count)
+            //            {
+            //                UserMenuList += "</ul>";
+            //                UserMenuList += "</li>";
+            //            }
+            //        }
+            //    }
+            //    UserMenuList += "</ul>";
+            //}
             return UserMenuList;
         }
 
