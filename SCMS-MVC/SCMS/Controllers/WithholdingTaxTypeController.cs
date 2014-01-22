04769914e9a0d18b7f2bb3f8874477fd3529ef48
Awais <awais@nevisol.com>
2014-01-23 01:30:54 +0500

    MA-HomePage-Slider

--- SCMS-MVC/SCMS/Controllers/WithholdingTaxTypeController.cs
+++ SCMS-MVC/SCMS/Controllers/WithholdingTaxTypeController.cs
@@ -20,100 +20,100 @@ namespace SCMS.Controllers
             return View("WithholdingTaxType");
         }
 
-        // Insertion
-        public ActionResult SaveRecord(String ps_Code, String Title)
-        {
-            SETUP_WithholdingTaxType lrow_WithholdingTaxType = new SETUP_WithholdingTaxType();
-            String ls_Action = "Edit", IsAuditTrail = "", ls_UserId = "";
-            String[] ls_Lable = new String[2], ls_Data = new String[2];
-            Int32 li_ReturnValue = 0;
+        //// Insertion
+        //public ActionResult SaveRecord(String ps_Code, String Title)
+        //{
+        //    SETUP_WithholdingTaxType lrow_WithholdingTaxType = new SETUP_WithholdingTaxType();
+        //    String ls_Action = "Edit", IsAuditTrail = "", ls_UserId = "";
+        //    String[] ls_Lable = new String[2], ls_Data = new String[2];
+        //    Int32 li_ReturnValue = 0;
 
-            try
-            {
-                if (String.IsNullOrEmpty(ps_Code))
-                {
-                    if (DALCommon.AutoCodeGeneration("SETUP_WithholdingTaxType") == 1)
-                    {
-                        ps_Code = DALCommon.GetMaximumCode("SETUP_WithholdingTaxType");
-                        ls_Action = "Add";
-                    }
-                }
+        //    try
+        //    {
+        //        if (String.IsNullOrEmpty(ps_Code))
+        //        {
+        //            if (DALCommon.AutoCodeGeneration("SETUP_WithholdingTaxType") == 1)
+        //            {
+        //                ps_Code = DALCommon.GetMaximumCode("SETUP_WithholdingTaxType");
+        //                ls_Action = "Add";
+        //            }
+        //        }
 
 
-                if (!String.IsNullOrEmpty(ps_Code))
-                {
-                    lrow_WithholdingTaxType.CldrType_Id = ps_Code;
-                    lrow_WithholdingTaxType.CldrType_Code = ps_Code;
-                    lrow_WithholdingTaxType.CldrType_Title = Title;
-                    lrow_WithholdingTaxType.CldrType_Active = 1;
+        //        if (!String.IsNullOrEmpty(ps_Code))
+        //        {
+        //            lrow_WithholdingTaxType.CldrType_Id = ps_Code;
+        //            lrow_WithholdingTaxType.CldrType_Code = ps_Code;
+        //            lrow_WithholdingTaxType.CldrType_Title = Title;
+        //            lrow_WithholdingTaxType.CldrType_Active = 1;
 
-                    li_ReturnValue = objDalWithholdingTaxType.SaveRecord(lrow_WithholdingTaxType);
-                    ViewData["SaveResult"] = li_ReturnValue;
+        //            li_ReturnValue = objDalWithholdingTaxType.SaveRecord(lrow_WithholdingTaxType);
+        //            ViewData["SaveResult"] = li_ReturnValue;
 
-                    IsAuditTrail = System.Configuration.ConfigurationManager.AppSettings.GetValues("IsAuditTrail")[0];
+        //            IsAuditTrail = System.Configuration.ConfigurationManager.AppSettings.GetValues("IsAuditTrail")[0];
 
-                    // Save Audit Log
-                    if (li_ReturnValue > 0 && IsAuditTrail == "1")
-                    {
-                        DALAuditLog objAuditLog = new DALAuditLog();
+        //            // Save Audit Log
+        //            if (li_ReturnValue > 0 && IsAuditTrail == "1")
+        //            {
+        //                DALAuditLog objAuditLog = new DALAuditLog();
 
-                        ls_UserId = ((SECURITY_User)Session["user"]).User_Id;
-                        ls_Lable[0] = "Code";
-                        ls_Lable[1] = "Title";
+        //                ls_UserId = ((SECURITY_User)Session["user"]).User_Id;
+        //                ls_Lable[0] = "Code";
+        //                ls_Lable[1] = "Title";
                        
-                        ls_Data[0] = ps_Code;
-                        ls_Data[1] = Title;
+        //                ls_Data[0] = ps_Code;
+        //                ls_Data[1] = Title;
                         
-                        objAuditLog.SaveRecord(13, ls_UserId, ls_Action, ls_Lable, ls_Data);
-                    }
-                }
+        //                objAuditLog.SaveRecord(13, ls_UserId, ls_Action, ls_Lable, ls_Data);
+        //            }
+        //        }
 
-                return PartialView("GridData");
-            }
-            catch
-            {
-                return PartialView("GridData");
-            }
-        }
+        //        return PartialView("GridData");
+        //    }
+        //    catch
+        //    {
+        //        return PartialView("GridData");
+        //    }
+        //}
 
-         public ActionResult DeleteRecord(String _pId)
-        {
-            String ls_Action = "Delete", IsAuditTrail = "", ls_UserId = "";
-            String[] ls_Lable = new String[2], ls_Data = new String[2];
-            Int32 li_ReturnValue = 0;
+        // public ActionResult DeleteRecord(String _pId)
+        //{
+        //    String ls_Action = "Delete", IsAuditTrail = "", ls_UserId = "";
+        //    String[] ls_Lable = new String[2], ls_Data = new String[2];
+        //    Int32 li_ReturnValue = 0;
 
-            try
-            {
-                SETUP_WithholdingTaxType WithholdingTaxTypeRow = objDalWithholdingTaxType.GetAllRecords().Where(c => c.CldrType_Id.Equals(_pId)).SingleOrDefault();
+        //    try
+        //    {
+        //        SETUP_WithholdingTaxType WithholdingTaxTypeRow = objDalWithholdingTaxType.GetAllRecords().Where(c => c.CldrType_Id.Equals(_pId)).SingleOrDefault();
 
-                li_ReturnValue = objDalWithholdingTaxType.DeleteRecordById(_pId);
-                ViewData["SaveResult"] = li_ReturnValue;
+        //        li_ReturnValue = objDalWithholdingTaxType.DeleteRecordById(_pId);
+        //        ViewData["SaveResult"] = li_ReturnValue;
 
-                IsAuditTrail = System.Configuration.ConfigurationManager.AppSettings.GetValues("IsAuditTrail")[0];
+        //        IsAuditTrail = System.Configuration.ConfigurationManager.AppSettings.GetValues("IsAuditTrail")[0];
 
-                // Delete Audit Log
-                if (li_ReturnValue > 0 && IsAuditTrail == "1")
-                {
-                    DALAuditLog objAuditLog = new DALAuditLog();
+        //        // Delete Audit Log
+        //        if (li_ReturnValue > 0 && IsAuditTrail == "1")
+        //        {
+        //            DALAuditLog objAuditLog = new DALAuditLog();
 
-                    ls_UserId = ((SECURITY_User)Session["user"]).User_Id;
-                    ls_Lable[0] = "Code";
-                    ls_Lable[1] = "Title";
+        //            ls_UserId = ((SECURITY_User)Session["user"]).User_Id;
+        //            ls_Lable[0] = "Code";
+        //            ls_Lable[1] = "Title";
 
-                    ls_Data[0] = WithholdingTaxTypeRow.CldrType_Code;
-                    ls_Data[1] = WithholdingTaxTypeRow.CldrType_Title;
+        //            ls_Data[0] = WithholdingTaxTypeRow.CldrType_Code;
+        //            ls_Data[1] = WithholdingTaxTypeRow.CldrType_Title;
                    
-                    objAuditLog.SaveRecord(13, ls_UserId, ls_Action, ls_Lable, ls_Data);
-                }
-                // Audit Trail Section End
+        //            objAuditLog.SaveRecord(13, ls_UserId, ls_Action, ls_Lable, ls_Data);
+        //        }
+        //        // Audit Trail Section End
 
-                return PartialView("GridData");
-            }
-            catch
-            {
-                return PartialView("GridData");
-            }
-        }
+        //        return PartialView("GridData");
+        //    }
+        //    catch
+        //    {
+        //        return PartialView("GridData");
+        //    }
+        //}
 
     }
 }
--- SCMS-MVC/SCMS/Views/Home/Index.aspx
+++ SCMS-MVC/SCMS/Views/Home/Index.aspx
@@ -1,30 +1,23 @@
 <%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/SCMSMaster.Master"
     Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
 
+<asp:Content ID="Content3" ContentPlaceHolderID="HeadContenct" runat="server">
+    <link href="<%=ResolveUrl("~/") %>Slider/js-image-slider.css" rel="stylesheet" type="text/css" />
+    <script src="<%=ResolveUrl("~/") %>Slider/js-image-slider.js" type="text/javascript"></script>
+    <link href="<%=ResolveUrl("~/") %>Slider/generic.css" rel="stylesheet" type="text/css" />
+</asp:Content>
 <asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
     Home
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-    <!-- embedding SWF -->
-    <script src="../../Slider/swfobject.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        var flashvars = {};
-        flashvars.xml_file = "../../Slider/flashmo_225_photo_list.xml";
-        var params = {};
-        params.wmode = "transparent";
-        var attributes = {};
-        attributes.id = "slider";
-        swfobject.embedSWF("../../Slider/flashmo_225_bar_slider.swf", "flashmo_slider", "940", "400", "9.0.0", false, flashvars, params, attributes);
-    </script>
-    <!-- embedding SWF -->
-    <div align="center">
-        <div id="flashmo_slider">
-            <a href="http://www.adobe.com/go/getflashplayer">
-                <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif"
-                    alt="Get Adobe Flash player" />
-            </a>
-            <br />
-            <br />
+    <div id="sliderFrame">
+        <div id="slider">
+            <img src="../../Slider/image-slider-1.png" alt=""/>
+            <img src="../../Slider/image-slider-2.png" alt=""/>
+            <img src="../../Slider/image-slider-3.png" alt=""/>
+            <img src="../../Slider/image-slider-4.png" alt=""/>
+            <img src="../../Slider/image-slider-5.png" alt=""/>
         </div>
+        
     </div>
-</asp:Content>
+ </asp:Content>
